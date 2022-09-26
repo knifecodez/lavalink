1ee03c2eed665915450b945780dd3897661cafd4
knifecodez <111521307+knifecodez@users.noreply.github.com>
2022-09-26 06:37:00 +0000

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-
-curl "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev" -o Lavalink.jar
-echo Lavalink.jar is downloaded...
-
-java -jar Lavalink.jar
